e5edadb0eac1e89052ec18150490d76656c02730
grace <graceschorno@protonmail.com>
2023-03-28 10:04:33 -0700

    Initialize git

---
+++ examples/basic_test.rs
@@ -0,0 +1,29 @@
+use bevy::prelude::*;
+use knyst::{prelude::*, wavetable::WavetableOscillatorOwned, graph::NodeAddress};
+use project::{KnystAudioPlugin, AudioCommands, AudioStartupSet};
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(KnystAudioPlugin)
+        .add_startup_system(initialize_graph.after(AudioStartupSet))
+        .run();
+}
+
+fn initialize_graph(mut a_commands: ResMut<AudioCommands>, mut commands: Commands) {
+    let node0 = a_commands.push(
+        WavetableOscillatorOwned::new(Wavetable::sine()),
+        inputs!(("freq" : 440.)),
+    );
+    let modulator = a_commands.push(
+        WavetableOscillatorOwned::new(Wavetable::sine()),
+        inputs!(("freq" : 5.)),
+    );
+    let mod_amp = a_commands.push(Mult, inputs!((0 ; modulator.out(0)), (1 : 0.25)));
+    let amp = a_commands.push(
+        Mult,
+        inputs!((0 ; node0.out(0)), (1 : 0.5 ; mod_amp.out(0))),
+    );
+    a_commands.connect(amp.to_graph_out());
+    a_commands.connect(amp.to_graph_out().to_index(1));
+}
---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+use std::time::Duration;
+
+use bevy::{prelude::{Plugin, World, Resource, Deref, DerefMut, SystemSet, IntoSystemSetConfig, StartupSet, IntoSystemConfig}, tasks::IoTaskPool};
+use knyst::{
+    audio_backend::{CpalBackend, CpalBackendOptions}, 
+    prelude::{AudioBackend, Graph, GraphSettings, RunGraphSettings}, 
+    controller::KnystCommands
+};
+
+#[derive(SystemSet, Debug, Clone, Eq, PartialEq, Hash)]
+pub struct AudioStartupSet;
+
+pub struct KnystAudioPlugin;
+
+impl Plugin for KnystAudioPlugin {
+    fn build(&self, app: &mut bevy::prelude::App) {
+        app .configure_set(AudioStartupSet.in_base_set(StartupSet::Startup))
+            .add_startup_system(setup_knyst_graph.in_set(AudioStartupSet));
+    }
+}
+
+fn setup_knyst_graph(world: &mut World) {
+    let mut backend = CpalBackend::new(CpalBackendOptions::default()).expect("Error in cpal audio backend");
+    let num_outputs = backend.num_outputs();
+    let sample_rate = backend.sample_rate() as f32;
+    let block_size = backend.block_size().unwrap_or(64);
+    let resources = knyst::Resources::new(knyst::ResourcesSettings::default());
+    let graph = Graph::new(GraphSettings { block_size, sample_rate, num_outputs, ..Default::default()});
+    let mut controller = backend
+        .start_processing_return_controller(
+            graph, 
+            resources, 
+            RunGraphSettings {
+                ..Default::default()
+            }, 
+            knyst::controller::print_error_handler
+        ).expect("Error in audio backend");
+    let commands = controller.get_knyst_commands();
+    
+    IoTaskPool::get().spawn(async move {
+        loop {
+            while !controller.run(300) {}
+            std::thread::sleep(Duration::from_micros(1));
+        }
+    }).detach();
+
+    let commands = AudioCommands(commands);
+
+    world.insert_resource(commands);
+    world.insert_non_send_resource(backend);
+}
+
+#[derive(Resource, Deref, DerefMut)]
+pub struct AudioCommands(pub KnystCommands);
